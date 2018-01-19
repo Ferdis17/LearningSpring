bb885829160723345dbb5e82991f57c3e4033081
Ferdis17 <fmuriyesu@users.noreply.github.com>
2018-01-18 20:01:20 -0600

    ok
    starting

---
+++ GSMS/src/main/java/rw/kfh/gsms/run/GsmsApp.java
@@ -0,0 +1,18 @@
+package rw.kfh.gsms.run;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GsmsApp {
+
+	
+	public static void main(String[] args) {
+		
+		SpringApplication.run(GsmsApp.class, args);
+		
+		
+
+	}
+
+}
